599687454ec2f98b710d4e432340fd640d15120a
Luke Pickering <luketpickering@googlemail.com>
2019-03-22 16:35:07 -0400

    Many small tweaks to improve the build process, and hopefully make it build a relocatable copy of NEUT.

--- build_neut.sh
+++ build_neut.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-if [ -z ${1} ]; then
+RUNDIR=$(pwd)
+
+if [ -z "${1}" ]; then
   echo "Expected to be passed the path to a NEUT source distribution."
   exit 1
 fi
 
 NEUT_ROOT=$(readlink -f ${1})
+
+NEUT_INSTALL_PREFIX=${NEUT_ROOT}
+if [ ! -z "${2}" ]; then
+
+  if [ -e "${2}" ] && [ ! -d "${2}" ]; then
+    echo "[ERROR]: ${2} Appears to exist, but is not a directory."
+    exit 1
+  fi
+  NEUT_INSTALL_PREFIX=${2}
+fi
+  
+echo "Will install NEUT to ${NEUT_INSTALL_PREFIX}."
 
 # Check CERNLIB, if not found install
 if [ ! -e cernlib ]; then
@@ -67,10 +81,35 @@ if [ ! -e cernlib ]; then
   source setup_cernlib.sh
 else 
   source cernlib/setup_cernlib.sh
+
+  # Check root
+  if [ -z "`which root-config 2>/dev/null`" ]; then
+    echo "*** root-config was not found. Please install/set up the root environment."
+    echo ""
+    exit 1
+  fi
+
 fi
 
 # Go to NEUT
 cd ${NEUT_ROOT}
+
+# Check if the expected Nieves build dirs exist
+if [ ! -e src/n1p1h/bin ]; then
+  mkdir -p src/n1p1h/bin
+fi
+
+if [ ! -e src/n1p1h/lib ]; then
+  mkdir	-p src/n1p1h/lib
+fi
+
+if [ ! -e src/ht2p2h/lib ]; then
+  mkdir	-p src/ht2p2h/lib
+fi
+
+if [ ! -e src/ht2p2h/bin ]; then
+  mkdir	-p src/ht2p2h/bin
+fi
 
 # Check dirs exist
 if ! cd src/neutsmpl; then
@@ -95,18 +134,44 @@ if [ ! -e neutroot2 ]; then
   exit 1
 fi
 
-cd ${NEUT_ROOT}
+# make sure that you're where you started so that relative paths are
+# handled correctly.
+cd ${RUNDIR}
+
+echo "Installing to: ${NEUT_INSTALL_PREFIX}"
+
+if [ ! -e "${NEUT_INSTALL_PREFIX}" ]; then
+
+  if ! mkdir -p ${NEUT_INSTALL_PREFIX}; then
+    echo "Failed to make install prefix: ${NEUT_INSTALL_PREFIX}. neutroot2 appears to have built successfully in ${NEUT_ROOT}/src/neutsmpl though."
+    exit 1
+  fi
+
+fi
+
+cd ${NEUT_INSTALL_PREFIX}
+
+NEUT_INSTALL_PREFIX=$(readlink -f .)
 
 mkdir -p bin; cd bin
 cp ${NEUT_ROOT}/src/neutsmpl/neutroot2 ./
 cd ../
-mkdir -p example_cards; cd example_cards
+mkdir -p etc/neut; cd etc/neut
+mkdir -p cards; cd cards
 cp ${NEUT_ROOT}/src/neutsmpl/Cards/*.card ./
 cd ../
 mkdir -p crsdat; cd crsdat
 cp -r ${NEUT_ROOT}/src/crsdat/* .
-cd ../
 
-echo "Built NEUT, now source ${NEUT_ROOT}/setup_neut.sh"
+rm -r N1p1h/
+mkdir N1p1h
+cp -r ${NEUT_ROOT}/src/n1p1h/Tables_std N1p1h
 
-echo -e '#!/bin/bash'"\nexport NEUT_ROOT=${NEUT_ROOT}\nNEUT_CARDS=${NEUT_ROOT}/src/neutsmpl/Cards\nNEUT_CRSDAT=${NEUT_ROOT}/src/crsdat\nexport PATH=\${NEUT_ROOT}/bin;\${PATH}" > setup_neut.sh
+rm HT2p2h 
+cp -r ${NEUT_ROOT}/src/ht2p2h/HT2p2h ./
+
+cd ../../../
+
+echo "Built NEUT, now source ${NEUT_INSTALL_PREFIX}/bin/thisneut.sh"
+
+echo -e '#!/bin/bash'"\nexport NEUT_ROOT=${NEUT_INSTALL_PREFIX}\nNEUT_CARDS=\${NEUT_ROOT}/etc/neut/cards\nNEUT_CRSDAT=\${NEUT_ROOT}/etc/neut/crsdat\nexport PATH=\${NEUT_ROOT}/bin:\${PATH}" > bin/thisneut.sh
